4b52cb665e81d5dfdb3fcef7eda1d01e003644d8
curtinaz <curtinazif@gmail.com>
2020-06-02 09:49:28 -0300

    added routes.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.json ({ message: 'Hello World'})
-})
+app.use(routes);
 
 console.log('Server Online');
 app.listen(3333);
