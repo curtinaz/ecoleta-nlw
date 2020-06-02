18155de5e95f1f5f6fe765c605db3b46b38aec47
curtinaz <curtinazif@gmail.com>
2020-06-02 09:15:11 -0300

    User return

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,36 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+    'Cristian',
+    'Dayton',
+    'Rogerinho',
+    'Diego'
+];
+
 app.get('/users', (req, res) => {
 
-    res.json({"message": "hello world"});
+    res.json(users);
 
     console.log('Listagem de usuários');
-})
+});
+
+app.get('/users/:id', (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const user = users[id];
+
+    console.log('Works')
+
+    return res.json(user);
+});
+
+app.post ('/users', (req, res) => {
+
+    return res.json(users);
+
+});
 
 console.log('Server Online');
-
 app.listen(3333);
